0bbb868dcc50eade7c473133482e2b2aafda583f
Vignesh Hari <vichuhari100@gmail.com>
2018-10-07 17:38:18 +0530

    Initial pages done

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+new TypeIt('#example1', {
+  strings: 'Hi There   </pre> <i class="em marbt em-wave emojitype animated wobble infinite" ></i>     ',
+  speed: 100,
+  autoStart: false,
+  lifeLike: false,
+  blink:false,
+  cursor:false
+});
